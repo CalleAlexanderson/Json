5bc3b1a497c3194e9b99339f7de62a37aa7ab4e6
Calle Alexanderson <calle.alexanderson@elev.ga.ntig.se>
2021-03-12 09:12:18 +0100

    d

--- list.js
+++ list.js
@@ -9,10 +9,21 @@ fetch("./songs.json")
         data.forEach(song => {
             let newDiv = document.createElement("div");
             newDiv.classList.add("song")
-            let songTitle = document.createElement("h3");
+            let albumCover = document.createElement("img");
+            albumCover.setAttribute("src", "./img/" + song.img);
+            newDiv.append(albumCover);
+            let songTitle = document.createElement("h2");
             songTitle.innerHTML = song.title;
             newDiv.append(songTitle);
-            
+            let songArtist = document.createElement("h3")
+            songArtist.innerHTML = song.artist;
+            newDiv.append(songArtist);
+            let songAlbum = document.createElement("p")
+            songAlbum.innerHTML = song.album;
+            newDiv.append(songAlbum);
+            let songDate = document.createElement("p")
+            songDate.innerHTML = song.date;
+            newDiv.append(songDate);
             document.querySelector(".list").append(newDiv);
         });
     })
