6dad972d039d864b697ccc7ef3275df596c55863
Calle Alexanderson <calle.alexanderson@elev.ga.ntig.se>
2021-03-12 10:30:51 +0100

    Working

--- list.js
+++ list.js
@@ -10,19 +10,23 @@ fetch("./songs.json")
             let newDiv = document.createElement("div");
             newDiv.classList.add("song")
             let albumCover = document.createElement("img");
-            albumCover.setAttribute("src", "./img/" + song.img);
+            albumCover.setAttribute("src", "./images/" + song.img);
             newDiv.append(albumCover);
-            let songTitle = document.createElement("h2");
+            let songTitle = document.createElement("h3");
             songTitle.innerHTML = song.title;
+            songTitle.classList.add("text")
             newDiv.append(songTitle);
-            let songArtist = document.createElement("h3")
+            let songArtist = document.createElement("p")
             songArtist.innerHTML = song.artist;
+            songArtist.classList.add("text")
             newDiv.append(songArtist);
             let songAlbum = document.createElement("p")
             songAlbum.innerHTML = song.album;
+            songAlbum.classList.add("text")
             newDiv.append(songAlbum);
             let songDate = document.createElement("p")
             songDate.innerHTML = song.date;
+            songDate.classList.add("text")
             newDiv.append(songDate);
             document.querySelector(".list").append(newDiv);
         });
